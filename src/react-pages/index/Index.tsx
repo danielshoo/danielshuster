45ef159939ab88bf212f45693812e87ed76df978
Daniel Shuster <danielshuster+github@outlook.com>
2023-07-01 00:21:13 -0400

    [feature/IndexPage] Search bar view component added.
    Container component to be added in a subsequent commit to enable search logic

--- src/react-pages/index/Index.tsx
+++ src/react-pages/index/Index.tsx
@@ -2,19 +2,29 @@ const React = require('react');
 const IndexCss = require('./Index.scss');
 const IndexCssJsx = () => <style>{IndexCss}</style>;
 const ThemeToggle = require('./components/theme-toggle/ThemeToggle');
+const Search = require('./components/search/Search');
+const NavigationBar = require('./components/navigation-bar/NavigationBar');
+const NavMenu = require('./components/nav-menu/NavMenu');
 
 module.exports = function Index() {
     return <html>
         <head>
             <title>Daniel Shuster - A Resume</title>
+            <meta charSet="UTF-8"/>
             <script defer={true} src="/react-pages/main.js"></script>
             <IndexCssJsx/>
+            <ThemeToggle.Style/>
+            <NavigationBar.Style/>
+            <Search.Style/>
+            <NavMenu.Style/>
         </head>
         <body>
             <div id="root" className={'dsr-index'}>
-                <section className={'dsr-navigation-bar'}>
+                <NavigationBar>
+                    <NavMenu currentPage={'Resumé'}/>
+                    <Search/>
                     <ThemeToggle/>
-                </section>
+                </NavigationBar>
             </div>
         </body>
     </html>;
