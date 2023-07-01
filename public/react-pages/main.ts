0858a33ea08aad72c420a2ed96fdc0339d86e478
Daniel Shuster <danielshuster+github@outlook.com>
2023-07-01 00:21:14 -0400

    [feature/IndexPage] With the es-custom-sass-builder in place, refactored how the multiple scss dependencies are loaded. Now uses SCSS imports.
    Takes advantage of the SASS library's native module resolution (relative paths checked first) and promotes SCSS code reuse.

--- public/react-pages/main.ts
+++ public/react-pages/main.ts
@@ -1,3 +1,5 @@
+require('./../../src/react-pages/index/Index.scss');
+
 const react = require('react');
 const reactDom = require('react-dom');
 const root = document.getElementById('root');
--- src/react-pages/index/Index.tsx
+++ src/react-pages/index/Index.tsx
@@ -1,13 +1,11 @@
 const React = require('react');
 const IndexCss = require('./Index.scss');
-const IndexCssJsx = () => <style>{IndexCss}</style>;
 const ThemeToggle = require('./components/theme-toggle/ThemeToggle');
 const Search = require('./components/search/Search');
 const NavigationBar = require('./components/navigation-bar/NavigationBar');
 const NavMenu = require('./components/nav-menu/NavMenu');
 const ProfessionalSummary = require('./components/professional-summary/ProfessionalSummary');
 const KeySkills = require('./components/key-skills/KeySkills');
-const SectionBanner = require('./components/section-banner/SectionBanner');
 const WorkHistory = require('./components/work-history/WorkHistory');
 
 module.exports = function Index() {
@@ -17,15 +15,7 @@ module.exports = function Index() {
             <meta charSet="UTF-8"/>
             <meta name={'viewport'} content={'width=device-width, initial-scale=1'}/>
             <script defer={true} src="/react-pages/main.js"></script>
-            <IndexCssJsx/>
-            <ThemeToggle.Style/>
-            <NavigationBar.Style/>
-            <Search.Style/>
-            <NavMenu.Style/>
-            <ProfessionalSummary.Style/>
-            <KeySkills.Style/>
-            <SectionBanner.Style/>
-            <WorkHistory.Style/>
+            <link rel="stylesheet" href="/react-pages/main.css"/>
         </head>
         <body>
             <div id="root" className={'dsr-index'}>
