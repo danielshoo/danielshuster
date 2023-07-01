5a7b6a7f08ba6d5d8f42cc20ad50ecc294e62788
Daniel Shuster <danielshuster+github@outlook.com>
2023-07-01 00:21:14 -0400

    [feature/IndexPage] Added a custom esbuild plugin for sass compilation.
    Includes a reorganization of build elements into subdirectories so to reduce the visual real estate, promote a level of modularity between them, and of course, boost the esbuild script's legibility.

--- build/esbuild.js
+++ build/esbuild.js
@@ -1,78 +1,31 @@
-const fs = require('fs');
-const path = require('path');
 const process = require('node:process');
-const sass = require('sass');
 const esbuild = require('esbuild');
-const esSassPlugin = require("esbuild-plugin-sass");
 
+const esPublicBuildOptions = require('./build-options/PublicOptions');
+const esSrcBuildOptions = require('./build-options/SrcOptions');
 
 const isWatchMode = process.argv.includes('--watch');
-const isProductionMode = process.argv.includes('--prod');
-
-const srcMainFile = path.resolve(__dirname, '..', 'main.ts');
-const publicMainFile = path.resolve(__dirname, '..', 'public', 'react-pages', 'main.ts');
-
-const publicEsBuildOptions = {
-    platform: 'browser',
-    entryPoints: [publicMainFile],
-    target: 'es2020',
-    outdir: path.dirname(publicMainFile),
-    sourcemap: !isProductionMode,
-    tsconfig: path.resolve(__dirname, '..', 'tsconfig.json'),
-    bundle: true,
-    minify: isProductionMode,
-    plugins: [esSassPlugin()],
-}
-
-const srcEsBuildOptions = {
-    platform: 'node',
-    entryPoints: [srcMainFile],
-    outdir: path.dirname(srcMainFile),
-    sourcemap: true,
-    tsconfig: path.resolve(__dirname, '..', 'tsconfig.json'),
-    bundle: true,
-    minify: isProductionMode,
-    plugins: [{
-        name: 'sassToString',
-        setup: function (build) {
-
-            build.onResolve({ filter: /.scss$/ }, args => {
-
-                const sourceFullPath = require.resolve(args.path, {
-                    paths: [args.resolveDir],
-                });
-
-                return {
-                    path: sourceFullPath,
-                    namespace: 'file',
-                };
-            });
-
-            build.onLoad({ filter: /.s[ac]ss$/, namespace: 'file' }, (args) => {
-
-                const scssString = fs.readFileSync(args.path, {encoding: 'utf-8'});
-                const scssFileContents = sass.compileString(scssString);
-
-               return {
-                   contents: `module.exports = ${JSON.stringify(scssFileContents.css)}`
-               };
-            });
-        }
-    }],
-};
 
 if (isWatchMode) {
+
     (async () => {
-        const esbuildCTX = await esbuild.context(srcEsBuildOptions);
-        await esbuildCTX.watch().then(() => {
-            console.log('Watching for changes...');
-        });
-        const publicEsbuildCTX = await esbuild.context(publicEsBuildOptions);
-        await publicEsbuildCTX.watch().then(() => {
-            console.log('Watching for public changes...');
-        });
+
+        const esCtxPublic = await esbuild.context(esPublicBuildOptions);
+        await esCtxPublic.watch().then(() => { console.log('Watching for public changes...');});
+
+        const esCtxSrc = await esbuild.context(esSrcBuildOptions);
+        await esCtxSrc.watch().then(() => { console.log('Watching for src changes...'); });
+
     })();
 } else {
-    esbuild.build(srcEsBuildOptions).catch(() => process.exit(1));
-    esbuild.build(publicEsBuildOptions).catch(() => process.exit(1));
+
+    esbuild.build(esSrcBuildOptions).catch((e) => {
+        console.error(e);
+        process.exit(1)
+    });
+
+    esbuild.build(esPublicBuildOptions).catch((e) => {
+        console.error(e);
+        process.exit(1)
+    });
 }
